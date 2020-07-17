9775654d0e6008fab5d2f1cfe8ab7e9002a9359d
Patr076 <68442721+Patr076@users.noreply.github.com>
2020-07-17 21:30:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-// Modules
 const express = require('express');
 const bodyParser = require('body-parser');
  
-// Set port for server
 const port = process.env.PORT || 3100;
  
-// Express set up
 let app = express();
 app.use(bodyParser.json());
  
-// Start server
+app.post('/webhook', (req, res, next) => {
+	
+	let action = req.body.result.action;
+	let message = action === 'get.wp.content' ? `Hey, our webhook is connected!` : `Sorry, I didn't get that`;
+	
+	res.send({
+		speech: message,
+		displayText: message,
+		source: 'wp-webhook',
+	});
+	
+});
+ 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 });
